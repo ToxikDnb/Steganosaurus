8cf35c60571163901ae0e46bd0981dca437aad51
toxikdnb <mackenzieblackaby@outlook.com>
2025-05-27 15:09:20 +0100

    Implemented file handling functionality in the Main Backend, and modified action logic to reflect this

--- app/src/main/java/steganosaurus/Controllers/BackendActions.java
+++ app/src/main/java/steganosaurus/Controllers/BackendActions.java
@@ -1,6 +1,13 @@
 package steganosaurus.Controllers;
 
+import javax.swing.JFileChooser;
+import steganosaurus.GUI.MainWindow;
+import java.io.File;
+
 public class BackendActions extends Actions<BackendEnum> {
+
+    private int fileRepIndex = -1; // Index of the file represented by the button in the GUI. -1 unless modified by
+    // a constructor.
 
     /**
      * Constructor for BackendActions.
@@ -11,6 +18,17 @@ public class BackendActions extends Actions<BackendEnum> {
         super(actionType);
     }
 
+    /**
+     * Constructor for BackendActions with a specific file representation index.
+     *
+     * @param actionType   the type of backend action this instance represents
+     * @param fileRepIndex the index of the file represented by this action
+     */
+    public BackendActions(BackendEnum actionType, int fileRepIndex) {
+        super(actionType);
+        this.fileRepIndex = fileRepIndex;
+    }
+
     @Override
     public void execute() {
         switch (actionType) {
@@ -18,16 +36,28 @@ public class BackendActions extends Actions<BackendEnum> {
                 // Logic to switch modes
                 break;
             case SELECT_FILE:
-                // Logic to select a file
+                File[] selectedFiles = openFileChooser(false);
+                if (selectedFiles != null && selectedFiles.length > 0) {
+                    File selectedFile = selectedFiles[0];
+                    MainWindow.instance.getBackend().selectFile(selectedFile);
+                }
+                break;
+            case SELECT_CARRIER:
+                if (fileRepIndex < 0)
+                    break;
+                MainWindow.instance.getBackend().selectCarrier(fileRepIndex);
                 break;
             case ADD_CARRIERS:
-                // Logic to add carriers
+                File[] carrierFiles = openFileChooser(true);
+                if (carrierFiles == null || carrierFiles.length == 0)
+                    break;
+                MainWindow.instance.getBackend().addCarriers(carrierFiles);
                 break;
             case REMOVE_CARRIER:
-                // Logic to remove a carrier
+                MainWindow.instance.getBackend().deleteCarrier();
                 break;
             case CLEAR_CARRIERS:
-                // Logic to clear carriers
+                MainWindow.instance.getBackend().clearCarriers();
                 break;
             case RUN:
                 // Logic to run the backend process
@@ -36,5 +66,28 @@ public class BackendActions extends Actions<BackendEnum> {
                 throw new UnsupportedOperationException("Action not supported: " + actionType);
         }
     }
+
+    // #region Helper Functions
+
+    /**
+     * Opens a file chooser dialog to select a file.
+     *
+     * @return the selected file, or null if no file was selected
+     */
+    private File[] openFileChooser(boolean multipleSelection) {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setMultiSelectionEnabled(multipleSelection);
+        int returnValue = fileChooser.showOpenDialog(MainWindow.instance.getMainFrame());
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            if (multipleSelection)
+                return fileChooser.getSelectedFiles();
+            else
+                return new File[] { fileChooser.getSelectedFile() };
+        }
+        return null;
+    }
+
+    // #endregion
 
 }
--- app/src/main/java/steganosaurus/Controllers/BackendEnum.java
+++ app/src/main/java/steganosaurus/Controllers/BackendEnum.java
@@ -3,6 +3,7 @@ package steganosaurus.Controllers;
 public enum BackendEnum {
     SWITCH_MODE,
     SELECT_FILE,
+    SELECT_CARRIER,
     ADD_CARRIERS,
     REMOVE_CARRIER,
     CLEAR_CARRIERS,
