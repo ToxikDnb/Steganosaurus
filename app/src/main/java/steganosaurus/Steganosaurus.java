0dadb64410215c6188e6e74c289b29569b820981
Mackenzie <54173553+ToxikDnb@users.noreply.github.com>
2025-05-30 19:51:56 +0100

    Created main framework for working with gradle

---
+++ app/src/main/java/steganosaurus/Steganosaurus.java
@@ -0,0 +1,12 @@
+/*
+ * Main application class for Steganosaurus.
+ * This class serves as the entry point for the application.
+ */
+package steganosaurus;
+
+public class Steganosaurus {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Steganosaurus!");
+    }
+}
